003ed54401b70a769cb121ec863af04fc3ef5002
Therydusone <przem.pajak@gmail.com>
2019-11-15 14:41:50 +0100

    Updates

---
+++ precache-manifest.3fbe0b0bbe1be88e3e70c24a0b4dd235.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c061313abc79ee5069404b63207be891",
+    "url": "/first-app/index.html"
+  },
+  {
+    "revision": "859c58bd4085ec48ffa6",
+    "url": "/first-app/static/css/main.0f7368d0.chunk.css"
+  },
+  {
+    "revision": "d18032704ab2ea29f0bb",
+    "url": "/first-app/static/js/2.360eadc1.chunk.js"
+  },
+  {
+    "revision": "859c58bd4085ec48ffa6",
+    "url": "/first-app/static/js/main.18edff3a.chunk.js"
+  },
+  {
+    "revision": "37a0f4a5e65d0a4c15a1",
+    "url": "/first-app/static/js/runtime-main.5713801e.js"
+  }
+]);
